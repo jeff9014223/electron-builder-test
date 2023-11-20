384e3546def46033de7863116d53fa150cf9b6b7
Jeff <xeonnode@gmail.com>
2023-11-20 14:34:21 +0100

    Update

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+const { updateElectronApp } = require("update-electron-app")
 const { app, BrowserWindow } = require("electron")
 const path = require("node:path")
+
+updateElectronApp({
+    repo: "https://github.com/jeff9014223/electron-builder-test",
+    updateInterval: "5 minutes",
+})
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1024,
         height: 600,
+        resizable: false,
+        autoHideMenuBar: true,
+        disableAutoHideCursor: true,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
         }
